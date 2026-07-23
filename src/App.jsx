dcecb238945e9a44d5613b044c561d9c53f366bc
Filippo Graziano <filippograziano13@gmail.com>
2026-07-23 16:25:41 +0200

    add list of films add filter for the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import './App.css'
+import { useState } from 'react'
+import { films } from './components/data'
 
 const App = () => {
+
+  const [filter, setFilter] = useState(``);
+
   return (
     <>
-      
+
+      <input type="text" placeholder='Search' value={filter} onChange={e => setFilter(e.target.value)} />
+
+      <ul>
+
+        {/*r Assunzione: i titoli dei film sono univoci */}
+
+        {films.filter(films => films.title.toLocaleLowerCase().includes(filter)).map(el => (
+
+          <li key={el.title}>
+
+            <h2>{el.title}</h2>
+            <p>{el.genre}</p>
+
+          </li>
+
+        ))}
+
+      </ul>
     </>
   )
 }
